7f1a244f16cf58b750ede364c56808eee0349bfa
zakihoso <zakaria.zahouani.mail@gmail.com>
2023-06-15 07:00:15 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,6 @@ int main(void)
 	long int c;
 	long long int d
 	float f;
-
 printf("Size of an char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of an long int: %lu byte(s)\n", (unsigned long)sizeof(c));
