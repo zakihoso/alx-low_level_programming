f1974c2eabe13ebea095eee1422bf4bb209ef262
zakihoso <zakaria.zahouani.mail@gmail.com>
2023-08-01 15:40:10 -0700

    up

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the element
+ * print_listint - prints all the element of a linked list
  * @h: linked list of type listint_t to print
  *
  * Return: number of nodes
